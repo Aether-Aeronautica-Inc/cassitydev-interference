72a79ccdd3291c84f69707d360ae1cf6536288a7
Niwatori <sx.niwatori.xs@gmail.com>
2025-07-14 20:51:26 +0700

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import { loadMemory } from './handlers/memory.js';
 
 // Environment variable
 import dotenv from 'dotenv';
-dotenv.config(/** { path: "/etc/secrets/.env" } **/);
+dotenv.config({ path: "/etc/secrets/.env" });
 
 // Keep process alive
 import './others/dev-panel.js' // Build information & dev access panel
